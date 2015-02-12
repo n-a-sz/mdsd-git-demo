21c64335814e7d3d954afe8e4cb00adaa352d744
meres <meres@192.168.174.137>
2015-02-12 08:25:46 +0100

    This is a descriptive commit meesage.

--- hu.bme.ftsrg.mdsd.gitexample/src/hu/bme/ftsrg/mdsd/gitexample/Main.java
+++ hu.bme.ftsrg.mdsd.gitexample/src/hu/bme/ftsrg/mdsd/gitexample/Main.java
@@ -3,6 +3,6 @@ package hu.bme.ftsrg.mdsd.gitexample;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world! :)");
 	}
 }
